140f7002f06d6457831aa72ff5d8eee607fb5e68
jo17-dev <procompter1@gmail.com>
2023-08-07 21:35:20 -0400

    learnded more about the props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './app.css';
 function App(){
     return (
     <div className="section">
+        <List />
         <Job salary={50000} position="web ingrator" company="fatfish" className="section"/>
         <Job salary={80000} position="intermediate react developer" company="fatfish" />
         <Job salary={120000} position="Full Stack developer" company="CGI" />
@@ -29,6 +30,24 @@ const Job = (props) =>{
             <h2>Statut: <strong style={{color : isGreen ? "green" : "red"}}> {statut} </strong> </h2>
         </section>
     </>);
+}
+
+const List =() =>{
+    var names = [
+        "Joel",
+        "Bertrand",
+        "tsafack",
+        "Bobo",
+        "Meli"
+    ];
+
+    return (
+        <ul>
+            {names.map((value, key)=>{
+                return <li key={key}> {value} </li>
+            })}
+        </ul>
+    )
 }
 // export The main component
 export default App;
